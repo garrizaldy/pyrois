ecd11f58797fd854791494ad789ed919d000cd81
Garrizaldy Santos <garrizaldy.santos@phpugph.com>
2014-05-07 18:55:23 +0800

    Initial Commit.

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('pyroisApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
